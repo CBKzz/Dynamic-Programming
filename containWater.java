f75361929d98b30c558511a4e0564b9c3f18f54c
CBKzz <baokuncheng@gmail.com>
2016-02-25 22:41:52 -0500

    Added files via upload

---
+++ containWater.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        if (height.length<2) return 0;
+        int lo=0;
+        int hi=height.length-1;
+        int max=Integer.MIN_VALUE;
+        while(lo<hi){
+            max=Math.max(max,(hi-lo)*Math.min(height[lo],height[hi]));
+            if(height[lo]<height[hi]) lo++;
+            else hi--;
+        }
+        return max;
+    }
+}
